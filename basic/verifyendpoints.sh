1566efc811dd81bbe6305e617e12d67ac1998328
mihirpandya-greenops <84353489+mihirpandya-greenops@users.noreply.github.com>
2022-02-10 15:08:01 -0800

    Update verifyendpoints.sh

--- basic/verifyendpoints.sh
+++ basic/verifyendpoints.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-# sleep 15
-# echo "Test failed"
-# exit 1
-sleep 10
-for i in 1 2 3 4 5
-do
-   echo $SERVICE_INTERNAL_URL
-   resp=$(curl $SERVICE_INTERNAL_URL:5000/)
-   if [ "$resp" != "Hello, World!" ]; then
-      echo "Did not return correctly."
-      exit 1;
-   fi
-   sleep 1
-done
-echo "Test completed correctly."
+sleep 15
+echo "Test failed"
+exit 1
+# sleep 10
+# for i in 1 2 3 4 5
+# do
+#    echo $SERVICE_INTERNAL_URL
+#    resp=$(curl $SERVICE_INTERNAL_URL:5000/)
+#    if [ "$resp" != "Hello, World!" ]; then
+#       echo "Did not return correctly."
+#       exit 1;
+#    fi
+#    sleep 1
+# done
+# echo "Test completed correctly."
